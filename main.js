6631602a97217b8172ddbceae1884360a56c5067
HeoJongWoon <whddns0902@gmail.com>
2023-10-01 23:54:51 +0900

    시간에 따라서 클래스 전환

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-/*
-	특정 시간을 주기로 코드를 반복할때
-	setInterval(반복실행할 함수, 반복간격시간(ms))
-*/
 const [hr, m, s] = document.querySelectorAll('h1 span');
-
 setInterval(setTime, 1000);
 
+//함수 추가
+//원하는 시간마다 body에 morning, afternoon, evening, night 클래스 추가
+//if(hour>=6 && hours< 11) document.body.classList.add('morging')
+//주의점 밤은 특정이상 혹은 일정시간 이하 (&&가 아닌 ||로 표현가능)
+
+function changeClass(hr) {
+	if (hr >= 6 && hr < 12) document.body.classList.add('morning');
+	if (hr >= 12 && hr < 16) document.body.classList.add('afternoon');
+	if (hr >= 16 && hr < 20) document.body.classList.add('evening');
+	if (hr >= 20 || hr < 6) document.body.classList.add('night');
+}
 function setTime() {
 	const now = new Date();
 	const hours = now.getHours();
@@ -14,8 +20,8 @@ function setTime() {
 	hr.innerText = setNumbers(setHours(hours));
 	m.innerText = setNumbers(min);
 	s.innerText = setNumbers(sec);
+	changeClass(hours);
 }
-
 function setNumbers(num) {
 	num < 10 && (num = '0' + num);
 	return num;
