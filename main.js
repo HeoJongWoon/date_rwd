53a9777da3e1446be39359592104f97c05e6f5cb
HeoJongWoon <whddns0902@gmail.com>
2023-10-01 23:51:42 +0900

    시간값 가공

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
+const [hr, m, s] = document.querySelectorAll('h1 span');
+
 const now = new Date();
 const hours = now.getHours();
 const min = now.getMinutes();
 const sec = now.getSeconds();
 
-console.log(setNumbers(hours));
+//hours값을 setHours가 조건에 12를 뺀값으로 반환
+//반환된 결과값을 다시 바로 setNumbers에 집어넣어서 0을 붙이도록 처리
+hr.innerText = setNumbers(setHours(hours));
+m.innerText = setNumbers(min);
+s.innerText = setNumbers(sec);
 
 function setNumbers(num) {
-	return num < 10 && (num = '0' + num);
+	num < 10 && (num = '0' + num);
+	return num;
 }
 
-//const setNumbers = num => num < 10 && (num = '0' + num);
+function setHours(num) {
+	num > 12 && (num = num - 12);
+	return num;
+}
