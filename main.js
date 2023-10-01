e2b4e44ab203de1d36f73d42eeb38cd1eed7e7c0
HeoJongWoon <whddns0902@gmail.com>
2023-10-01 23:51:01 +0900

    화살표함수로 변경

--- main.js
+++ main.js
@@ -6,33 +6,7 @@ const sec = now.getSeconds();
 console.log(setNumbers(hours));
 
 function setNumbers(num) {
-	/*
-	if (num < 10) num = '0' + num;	
-	*/
-	//조건식 ? true일때 실행할 구문 : false일때 실행할 구문
-	//삼항연산자 구문안에는 대입연산자가 들어올수 없기 때문에 괄호로 묶어서 표현식으로 변경
-	//특정값이 else 일때 굳이 실행할 코드가 없다면 &&연산자 활용
-	//num < 10 ? (result = '0' + num) : (result = num);
-	num < 10 && (num = '0' + num);
-
-	return num;
+	return num < 10 && (num = '0' + num);
 }
 
-/*
-let newHr = 0;
-if (hours > 12) {
-	newHr = hours - 12;
-	if (newHr < 10) {
-		newHr = '0' + newHr + 'pm';
-	} else {
-		newHr = newHr + 'pm';
-	}
-} else {
-	newHr = hours;
-	if (newHr < 10) {
-		newHr = '0' + newHr + 'am';
-	} else {
-		newHr = newHr + 'am';
-	}
-}
-*/
+//const setNumbers = num => num < 10 && (num = '0' + num);
