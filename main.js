83588757d6b20d051e2f902df97b85c40ecaf8d1
HeoJongWoon <whddns0902@gmail.com>
2023-10-01 23:53:31 +0900

    시간값 출력 반복처리

--- main.js
+++ main.js
@@ -1,28 +1,23 @@
+/*
+	특정 시간을 주기로 코드를 반복할때
+	setInterval(반복실행할 함수, 반복간격시간(ms))
+*/
 const [hr, m, s] = document.querySelectorAll('h1 span');
 
-const now = new Date();
-const hours = now.getHours();
-const min = now.getMinutes();
-const sec = now.getSeconds();
-
-//hours값을 setHours가 조건에 12를 뺀값으로 반환
-//반환된 결과값을 다시 바로 setNumbers에 집어넣어서 0을 붙이도록 처리
-hr.innerText = setNumbers(setHours(hours));
-m.innerText = setNumbers(min);
-s.innerText = setNumbers(sec);
-
-/* 다음과 같이 코드를 줄일수 있음
-const values = [setHours(hours), min, sec];
-spans.forEach((el, idx) => {
-	el.innerText = setNumbers(values[idx]);
-});
-*/
+setInterval(() => {
+	const now = new Date();
+	const hours = now.getHours();
+	const min = now.getMinutes();
+	const sec = now.getSeconds();
+	hr.innerText = setNumbers(setHours(hours));
+	m.innerText = setNumbers(min);
+	s.innerText = setNumbers(sec);
+}, 1000);
 
 function setNumbers(num) {
 	num < 10 && (num = '0' + num);
 	return num;
 }
-
 function setHours(num) {
 	num > 12 && (num = num - 12);
 	return num;
